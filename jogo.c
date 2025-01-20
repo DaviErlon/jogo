9afaeb09f907e1ee012c756fa331b5fde89af2ce
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-01-20 04:24:36 -0300

    codei a parte mais básica do código

--- jogo.c
+++ jogo.c
@@ -8,6 +8,40 @@
 #include "hardware/i2c.h"
 #include <time.h>
 
+#define I2C_SDA 14
+#define I2C_SCL 15
+
 int main(){
     
+    stdio_init_all();
+
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    while(true){
+        
+    }
+
 }
